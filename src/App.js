dd50778af92308d3cf5374e10ab283d991927e14
Donna <donnamcla@gmail.com>
2018-11-08 21:59:20 -0500

    add state and style to dropdown component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@ import logo from './logo@2x.png';
 import './dropdown.css'
 import './App.css';
 
+const options = [ 'objects', 'collections', 'index' ]
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: '',
+    };
+    this._onSelect = this._onSelect.bind(this)
+  }
+  _onSelect(option) {
+    console.log('You selected ', option.label)
+    this.setState({selected: option})
+  }
+
+  fetchContent = () => {
+    return fetch('')
+  }
+  
   render() {
-    const options = [ 'objects', 'collections', 'index' ]
-    
+        
     return (
       <div className="App">
+        <img src={logo} alt="healthcare.gov logo"/>
         <header className="App-header">
-          <img src={logo} alt="healthcare.gov logo"/>
+          <h1>Content Glossary</h1>
         </header>
-        <Dropdown options={options} onChange={this._onSelect} placeholder="Select an option" className="dropdown"/>
+        <Dropdown 
+          options={options} 
+          onChange={this._onSelect}
+          placeholder="Select an option" 
+          className="dropdown"
+          value={this.state.selected}
+        />
+        <ul id="content"></ul>
       </div>
     );
   }
