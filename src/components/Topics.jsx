e1b5aa0aca1d32bcd8455005b1b26ceacf10de0f
Donna <donnamcla@gmail.com>
2018-11-25 10:38:29 -0500

    remove some junk

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import Topic from './Topic';
 
 const Topics = (props) => {
-  
-  // console.log(props.topics.topics);
-
-  // const topicsArray = Array.from(props.topics);
-  // console.log(topicsArray);
-  
-  // const topicsList = topicsArray.map(topic => {
   const topicsList = props.topics.map(topic => {
     return (
       <div>
