975e8fab3462c2171d039d7084606315d57fd96f
Donna <donnamcla@gmail.com>
2018-11-25 11:09:21 -0500

    sort the states alphabetically

--- src/components/StatesList.jsx
+++ src/components/StatesList.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import State from './State';
 
 const StatesList = (props) => {
-  const statesList = props.states.map(USstate => {
+  
+  const sortedStates = props.states.sort(function(state1, state2) {
+    if (state1.title > state2.title) {
+      return 1;
+    }
+    if (state1.title < state2.title) {
+      return -1;
+    }
+    return 0;
+  });
+
+  const statesList = sortedStates.map(USstate => {
     return (
       <div>
-        <State title={USstate.title} url={USstate.url} />
+        <State title={USstate.title} url={USstate.url} lang={USstate.lang}/>
       </div>
     )
   });
