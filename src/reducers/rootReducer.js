f0969632f37a3af668bb2ad1f9a3ef9dd5b1c08a
Donna <donnamcla@gmail.com>
2018-11-24 20:24:14 -0500

    typo

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import contentReducer from './contentReducer';
 
 const rootReducer = combineReducers({
-  cpntent: contentReducer,
+  content: contentReducer,
 })
 
 export default rootReducer;
