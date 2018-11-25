b806e055e54593a14d1fe59d1831584312f399fc
Donna <donnamcla@gmail.com>
2018-11-25 17:36:38 -0500

    adds tests for the Header component

---
+++ src/components/Header.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Header from './Header';
+import logo from '../logo@2x.png';
+
+
+configure({ adapter: new Adapter()}); // Enzyme is now connected
+
+describe('<Header />', () => {
+  it('should render the heading/title', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(<h1>Content Browser</h1>));
+  });
+
+  it('should render the logo', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find(<img src={logo}/>));
+  });
+});
