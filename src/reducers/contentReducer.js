f10019588f7b1cbc927976ece488554567388fa2
Donna <donnamcla@gmail.com>
2018-11-25 14:09:23 -0500

    add blog and question reducers

--- src/reducers/contentReducer.js
+++ src/reducers/contentReducer.js
@@ -2,7 +2,7 @@ import * as actionTypes from '../actions/actionTypes';
 
 export default function contentReducer(state = {
   articles: [],
-  blogs: [],
+  blog: [],
   questions: [],
   glossary: [],
   states: [],
@@ -12,27 +12,33 @@ export default function contentReducer(state = {
     
     case actionTypes.LOADING_TOPICS:
       return { ...state, loading: true, topics: [], };
-    
     case actionTypes.FETCH_TOPICS:
       return { ...state, loading: false, topics: action.payload, };
     
     case actionTypes.LOADING_STATES:
       return { ...state, loading: true, states: [], };
-    
     case actionTypes.FETCH_STATES:
       return { ...state, loading: false, states: action.payload, };
     
     case actionTypes.LOADING_GLOSSARY:
       return { ...state, loading: true, glossary: [], };
-    
     case actionTypes.FETCH_GLOSSARY:
       return { ...state, loading: false, glossary: action.payload, };
 
     case actionTypes.LOADING_ARTICLES:
       return { ...state, loading: true, articles: [], };
-    
     case actionTypes.FETCH_ARTICLES:
       return { ...state, loading: false, articles: action.payload, };
+
+    case actionTypes.LOADING_BLOG:
+      return { ...state, loading: true, blog: [], };
+    case actionTypes.FETCH_BLOG:
+      return { ...state, loading: false, blog: action.payload, };
+
+    case actionTypes.LOADING_QUESTIONS:
+      return { ...state, loading: true, questions: [], };
+    case actionTypes.FETCH_QUESTIONS:
+      return { ...state, loading: false, questions: action.payload, };
 
     default: return state;
   }
